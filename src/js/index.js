9300d9688760bddd620986f7fc40772efe3b2abc
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-02 11:42:24 +0200

    clase-02-julio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+// INSERCIÓN DE ELEMENTOS EN EL DOM
+
+const testElement = document.getElementById('test');
+
+const titleElement = document.createElement('h1');
+titleElement.textContent = 'Título nuevo';
+
+testElement.prepend(titleElement);
+
+console.log(titleElement);
+
+// EJERCICIOS
+
+/* - Usando este HTML, haz que al hacer click en el botón se vayan añadiendo items con el texto Item 1, Item 2, Item 3,... Los item deben llevar el número correspondiente independiente de con cuantos item empiece la lista- */
