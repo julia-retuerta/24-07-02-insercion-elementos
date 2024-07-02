ad825bc62898ad88eaf187845f7987587e6763df
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-02 23:14:06 +0200

    practica-casa-02-julio

--- src/js/index.js
+++ src/js/index.js
@@ -14,4 +14,4 @@ console.log(titleElement);
 
 // EJERCICIOS
 
-/* - Usando este HTML, haz que al hacer click en el botón se vayan añadiendo items con el texto Item 1, Item 2, Item 3,... Los item deben llevar el número correspondiente independiente de con cuantos item empiece la lista- */
+/* - Usando este HTML, haz que al hacer click en el botón se vayan añadiendo items con el texto Item 1, Item 2, Item 3,... Los item deben llevar el número correspondiente independientemente de con cuántos item empiece la lista- */
